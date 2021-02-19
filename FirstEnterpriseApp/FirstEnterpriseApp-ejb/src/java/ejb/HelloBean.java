8e66030f3dfd774367295863d1171506d9dcd9b8
kamlendu <kamlendup@gmail.com>
2021-02-19 15:07:56 +0530

    first commit

---
+++ FirstEnterpriseApp/FirstEnterpriseApp-ejb/src/java/ejb/HelloBean.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author root
+ */
+@Stateless
+public class HelloBean implements HelloBeanLocal {
+
+    @Override
+    public String sayHello(String name) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+      return "Hello World of EJB from "+ name +" !";
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+
+}
